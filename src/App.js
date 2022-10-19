71140c0c21587f9589ef5e8acc7d9d7d17fe4bda
HugoTse <hugotse8@gmail.com>
2022-10-19 00:15:54 -0700

    table exists

--- src/App.js
+++ src/App.js
@@ -1,8 +1,106 @@
+// import '@aws-amplify/core/ui-react/styles.css';
+import Amplify from "@aws-amplify/core"
+
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import { isCompositeComponent } from "react-dom/test-utils";
+// import { Auth, Hub } from "aws-amplify";
+// import Amplify from "aws-amplify";
 
 function App() {
+ 
+  const [records, setRecords] = useState([]);
+
+
+  // Fetch the records in the table
+  async function fetchRecords(){
+    const headers = {
+      "Content-Type": "application/json",
+    }
+    const apiResponse = await fetch('https://guxr3spqrd.execute-api.us-west-1.amazonaws.com/v4/get', {headers} )
+    // const apiResponse = await fetch('http://api.open-notify.org/astros.json', {headers} )
+    const apiResponseJSON = await apiResponse.json()
+    console.log(apiResponseJSON);
+    // const rs = apiResponseJSON.body
+    // console.log(rs)
+    // setRecords([...rs])
+    setRecords([...apiResponseJSON])
+  }
+  // fetchRecords();
+  // Fetch the records in the table: UseEffect
+  useEffect(() => { 
+    async function fetchRecords(){
+      const headers = {
+        "Content-Type": "application/json",
+      }
+      const apiResponse = await fetch('https://guxr3spqrd.execute-api.us-west-1.amazonaws.com/v4/get', {headers} )
+      // const apiResponse = await fetch('http://api.open-notify.org/astros.json', {headers} )
+      const apiResponseJSON = await apiResponse.json()
+      console.log(apiResponseJSON);
+      // const rs = apiResponseJSON.body
+      // console.log("This is rs: " + rs)
+      // setRecords([...rs])
+      setRecords([...apiResponseJSON])
+    }
+    fetchRecords()
+  }, []);
+
   return (
     <div className="App">
       <h1>Ransomware Defender</h1>
+
+      <h2><i>Stored Files</i></h2>
+
+      <table className='tableClass'>
+        <thead>
+        <tr className='rowClass'>
+          <td className='columnClass'>
+            <b>FILE</b>
+          </td>
+          <td className='columnClass'>
+            <b>TIMESTAMP</b>
+          </td>
+          <td className='columnClass'>
+            <b>HASH</b>
+          </td>
+          <td className='columnClass'>
+            <b>USER</b>
+          </td>
+        </tr>
+        </thead>
+
+      <tbody>
+
+   
+      {
+      records.length > 0 ? (
+        records.map((file) => 
+        <>
+        {/* <p>hello</p> */}
+        <tr className='rowClass'>
+          <td className='columnClass'>
+            {file.file.S}
+          </td>
+          <td className='columnClass'>
+            {file.timestamp.S}
+          </td>
+          <td className='columnClass'>
+            {file.hash.S}
+          </td>
+          <td className='columnClass'>
+            {file.user.S}
+          </td>
+        </tr>
+        </>
+        )) 
+        : 
+        (<></>)
+      }
+
+      </tbody>
+      </table>
  
     </div>
   );
